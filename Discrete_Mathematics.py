73f7de7c9ea6031fdc0d630754f104814bcb8392
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-04-12 15:55:04 -0400

    Add Birthday Problem function

--- Discrete_Mathematics.py
+++ Discrete_Mathematics.py
@@ -1,3 +1,5 @@
+import math
+
 
 # function to solve permutations of 'n' elements (Chapter 9.2)
 def permutations_calc(n, r):
@@ -15,6 +17,26 @@ def permutations_calc(n, r):
     return numerator/denominator
 
 
-print(permutations_calc(6,1))
+#print(permutations_calc(6,1))
+
+
+# Assuming that all years have 365 days and all birthdays occur with equal probability, how large must n be so that
+# in any randomly chosen group of n people, the probability that two or more have the same birthday is at least
+# 'target'?
+def birthday_problem(target):
+    n = 1
+    sub = 1
+    for i in range(364, 0, -1):
+        n = 365 - i
+        #print('this is n', n)
+        #print(i)
+        sub = sub * (i + 1)
+        r = 365 ** n
+        calc = (r - sub) / r
+        if calc >= target:
+            break
+    print(n)
+
+print(birthday_problem(.9))
 
 
